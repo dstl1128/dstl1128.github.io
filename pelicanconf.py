3cfcde44e9b4ab4b284a9f799ea1c44292d2da6e
dereks <derek.saw@onapp.com>
2015-08-02 22:14:03 +0800

    present more settings in pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,8 +35,8 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ('You can add links in your config file', '#'),
-    ('Another social link', '#'),
+    ('github', 'https://github.com/dstl1128'),
+    ('bitbucket', 'https://bitbucket.org/dereks'),
 )
 
 DEFAULT_PAGINATION = 7
@@ -47,3 +47,21 @@ DEFAULT_PAGINATION = 7
 THEME='./pelican-bootstrap3'
 
 TAG_CLOUD_MAX_ITEMS=20
+
+USE_PAGER=True
+BOOTSTRAP_FLUID=False
+DISPLAY_CATEGORY_IN_BREADCRUMBS=True
+BOOTSTRAP_NAVBAR_INVERSE=True
+DISPLAY_SERIES_ON_SIDEBAR=True
+DISPLAY_ARTICLE_INFO_ON_INDEX=False
+DISPLAY_TAGS_ON_SIDEBAR=True
+DISPLAY_CATEGORIES_ON_SIDEBAR=True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR=True
+RECENT_POST_COUNT=7
+
+HIDE_SIDEBAR=False
+
+USE_OPEN_GRAPH=True
+#OPEN_GRAPH_FB_APP_ID=
+
+
